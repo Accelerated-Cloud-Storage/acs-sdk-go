b76f27d56ecbbdeeb306168fc0b58fc75f7ac66f
Accelerated Cloud Storage <acceleratedcloudstorage.com@acceleratedcloudstorage.com>
2025-03-02 08:38:56 +0000

    Updating back to orginal file

--- examples/basic/basicWorkflow.go
+++ examples/basic/basicWorkflow.go
@@ -27,6 +27,7 @@ func main() {
 	}
 	defer acsClient.DeleteBucket(context, bucketName)
 
+	fmt.Println("Bucket created:", bucketName)
 	// Create a new object
 	objectName := "my-object"
 	objectData := []byte("Hello, World!")
@@ -35,19 +36,21 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Println("Object created:", objectName)
 	// Get the object
 	data, err := acsClient.GetObject(context, bucketName, objectName)
 	if err != nil {
 		panic(err)
 	}
 	println(string(data))
+	fmt.Println("Object retrieved:", objectName)
 
 	// Delete the object
 	err = acsClient.DeleteObject(context, bucketName, objectName)
 	if err != nil {
 		panic(err)
 	}
-
+	fmt.Println("Object deleted:", objectName)
 	// Delete the bucket
 	err = acsClient.DeleteBucket(context, bucketName)
 	if err != nil {
